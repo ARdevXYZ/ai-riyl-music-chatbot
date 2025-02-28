7f9aa9501c7b515bba9b47c81d64e945fb7d0470
Andrew Rosinski <andrew.rosinski@gmail.com>
2025-02-28 18:45:27 -0500

    color mode preference fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import localFont from 'next/font/local';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,6 +13,61 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const cyanFont = localFont({
+  src: [
+    {
+      path: '../public/fonts/Brown-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-cyan-font-brown',
+});
+
+const cyanFontTwo = localFont({
+  src: [
+    {
+      path: '../public/fonts/Akkurat-Normal.otf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-cyan-font-akkurat',
+});
+
+const cyanFontThree = localFont({
+  src: [
+    {
+      path: '../public/fonts/CircularStd-Book.otf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-cyan-font-circular',
+});
+
+const cyanFontFour = localFont({
+  src: [
+    {
+      path: '../public/fonts/GT-Haptik-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-cyan-font-gt-haptik',
+});
+
+const cyanFontFive = localFont({
+  src: [
+    {
+      path: '../public/fonts/Graphik-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-cyan-font-grafik',
 });
 
 export const metadata: Metadata = {
